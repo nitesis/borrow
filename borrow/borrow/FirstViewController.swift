83a64e8111602745b6fd611ea2b70dd0c33af287
Raphael Brunner <raphi.brunner@me.com>
2016-03-30 10:07:45 +0200

    initial project setup

---
+++ borrow/borrow/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  borrow
+//
+//  Created by Raphael Brunner on 30.03.16.
+//  Copyright © 2016 Raphael Brunner. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ borrow/borrow/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  borrow
+//
+//  Created by Raphael Brunner on 30.03.16.
+//  Copyright © 2016 Raphael Brunner. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
