b02c4f403e221d38aebd9c87c63daeaa8ef956cc
nitesis <viviane.bendjus@students.fhnw.ch>
2016-04-17 14:39:30 +0200

    configured title table view cell

---
+++ borrow/borrow/TitleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TitleTableViewCell.swift
+//  borrow
+//
+//  Created by soulsista on 17.04.16.
+//  Copyright © 2016 Raphael Brunner. All rights reserved.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
